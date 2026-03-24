988dc96b12159f5e1d60022690a59feb110c753f
JuanTryhardx <juanpablosierra53@gmail.com>
2026-03-24 09:11:47 -0500

    part 2
    esta en observacion

--- base_datos.py
+++ base_datos.py
@@ -4,8 +4,31 @@ class Base_datos:
         self.lista_botellas = []
         
     def agregar_botella(self, nuevo_obj):
-        self.lista_botella.append(nuevo_obj)
+        self.lista_botellas.append(nuevo_obj)
         print(nuevo_obj)
-        print(self.lista_botella[0])
+        print(f"Botella agregada en posicion {len(self.lista_botellas) -1}")
+        
+    def buscar_botella(self, indice):
+        if 0 <= indice < len(self.lista_botellas):
+            print(f"\n Botella en posicion {indice}: ")
+            self.lista_botellas[indice].ver_info()
+            return self.lista_botellas[indice]
+        else:
+            print(f" No existe botella en esa posicion {indice}")
+            return None
+        
+    def actualizar_capacidad(self, indice, nueva_capacidad):
+        botella = self.buscar_botella(indice)
+        if botella:
+            botella.set_capacidad(nueva_capacidad)
+            print(f"capacidad actualizada a: {nueva_capacidad}")
+            
+    def eliminar_botella(self, indice):
+        if 0 <= indice < len(self.lista_botellas):
+            self.lista_botellas.pop(indice)
+            print(f"Botella en posicion {indice} eliminada.")
+        else: 
+            print(f"No existe botella en posicion {indice}")                   
+            
         
         
--- codigo_principal.py
+++ codigo_principal.py
@@ -6,16 +6,36 @@ from base_datos import Base_datos
 bd_dt = Base_datos()
 
 botella = Botella("1 L", "plastico")
+vidrio = Vidrio("2 L", "vidrio", "gris")
+plastico = Plastico("1 L", "plastico", "plastica")
+
+
+
 print("-----------DATOS_BOTELLA-------------")
 botella.ver_info()
 
-vidrio = Vidrio("2 L", "vidrio", "gris")
 print("\n-----------DATOS_BOTELLA_VIDRIO--------")
 vidrio.ver_info()
 
-plastico = Plastico("1 L", "plastico", "plastica")
 print("\n-----------DATOS_BOTELLA_PLASTICO--------")
 plastico.ver_info()
 
-obj_base_dt.agregar_botella(obj_botella)
-print(obj_botella)
+#AGREGAR lA BASE DE DATOS
+bd_dt.agregar_botella(botella)
+bd_dt.agregar_botella(vidrio)
+bd_dt.agregar_botella(plastico)
+
+print("\n -----VER TODAS------")
+bd_dt.ver_todas()
+
+#actualizar capacidad
+print("\n--- ACTUALIZAR ID 1 ---")
+bd_dt.actualizar_capacidad(1, "3 L")
+
+#eliminar
+print("\n--- ELIMINAR ID 3 ---")
+bd_dt.eliminar_botella(3)
+
+#final
+print("\n--- BASE DE DATOS FINAL ---")
+bd_dt.ver_todas()
